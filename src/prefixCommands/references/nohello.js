296eef87a6cee52e991f2d78d0d51bba8b078352
Caleb Ephrem <quantumpandaprog@gmail.com>
2026-03-07 07:19:48 +0000

    refactor(prefixCommands): remove references now handled by pawgrammer bot

--- src/prefixCommands/references/nohello.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "nohello",
-  description: "nohello.net",
-  callback: async (client, message, args) => {
-    try {
-      return message.channel.send("https://nohello.net/");
-    } catch (err) {}
-  },
-};
